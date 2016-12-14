9fa1f844a94a1a3a4d6137cd134fe2ea0e9ba99d
tomhuhges <merzbear@gmail.com>
2016-12-14 08:00:10 +0000

    tweak soundboard

---
+++ src/components/projects/one/Tracks.js
@@ -0,0 +1,25 @@
+import AIRHORN from 'file!./audio/AIRHORN.mp3'
+import SANIC from 'file!./audio/SANIC.mp3'
+import DAMNSON from 'file!./audio/DAMNSON.mp3'
+import OOOOOOOOHMYGOOOOD from 'file!./audio/OOOOOOOOHMYGOOOOD.mp3'
+import SKRILLEX from 'file!./audio/SKRILLEX.mp3'
+import HITMARKER from 'file!./audio/HITMARKER.mp3'
+import SMOKEWEEDEVERYDAY from 'file!./audio/SMOKEWEEDEVERYDAY.mp3'
+import AIRPORN from 'file!./audio/AIRPORN.mp3'
+import TWOSAD4ME from 'file!./audio/2SAD4ME.mp3'
+import THREESPOOKY5ME from 'file!./audio/3SPOOKY5ME.mp3'
+
+const AudioFiles = [
+	{ 'a': AIRHORN },
+	{ 's': SANIC },
+	{ 'd': DAMNSON },
+	{ 'f': OOOOOOOOHMYGOOOOD },
+	{ 'g': SKRILLEX },
+	{ 'h': HITMARKER },
+	{ 'j': SMOKEWEEDEVERYDAY },
+	{ 'k': AIRPORN },
+	{ 'l': TWOSAD4ME },
+	{ 'semicolon': THREESPOOKY5ME }
+]
+
+export default AudioFiles
